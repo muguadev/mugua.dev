c793772dc698ecbe3314438b57c92f115f7b8a3a
muguā <mugua.developer@gmail.com>
2020-10-22 14:36:42 +0200

    Creates lipsum content for option 1

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,29 @@
+const allOptions = document.querySelectorAll(".menu .option");
+
+function show(pageId) {
+  console.log(`Option ${pageId} has been clicked`);
+  document.querySelector(`#${pageId}`).classList.remove("hidden");
+}
+
 function clicked(ev) {
-  alert(`Option ${ev.target.getAttribute("data-option")} has been clicked`);
+  const target = ev.target;
+  for (o of allOptions) {
+    o.classList.add("hidden");
+  };
+  show(target.getAttribute("data-option"));
 }
 
 function setOptionHandlers() {
   console.log("Attaching event handlers to menu options");
-  for (o of document.querySelectorAll(".menu .option")) {
+  for (o of allOptions) {
     o.addEventListener('click', clicked)
   }
+}
+
+function hidePage(target) {
+  console.log(`Closing ${target.getAttribute("id")}`)
+  target.parentElement.classList.add("hidden");
+  for (o of allOptions) {
+    o.classList.remove("hidden");
+  };
 }
